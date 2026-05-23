7d237331ba47f7b9adceee1c00f4644f488681bc
UnpolishedNoob <banikpritom321@gmail.com>
2026-05-24 02:55:51 +0600

    Added loading design

---
+++ src/components/Loadingcard.jsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "@heroui/react";
+const Loadingcard = () => {
+  return (
+    <div>
+      <div className="space-y-2">
+        <p className="truncate text-xs text-muted">Loading...</p>
+        <div className="shadow-panel space-y-3 rounded-lg bg-transparent p-4">
+          <Skeleton animationType="pulse" className="h-20 rounded-lg" />
+          <Skeleton animationType="pulse" className="h-3 w-3/5 rounded-lg" />
+          <Skeleton animationType="pulse" className="h-3 w-4/5 rounded-lg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loadingcard;
