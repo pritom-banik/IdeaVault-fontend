54c70241084cd1180a6162fe006feb6780715141
UnpolishedNoob <banikpritom321@gmail.com>
2026-05-26 21:46:19 +0600

    Added BetterAuth JWT in frontend

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins"
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("ideavault");
@@ -21,4 +22,14 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
         }, 
     },
+    session:{
+      cookieCache:{
+        enabled:true,
+        strategy:"jwt",
+        maxAge:6*24*60*60
+      }
+    },
+  plugins: [
+        jwt(), 
+    ]
 });
